f830aaee24c0341663940e43d18863f840172aff
Musthafa Faishal <musthafafaishal@gmail.com>
2024-07-07 16:42:01 +0000

    refactor(challenges page): change the fetch endpoint for challenges

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,11 +1,11 @@
 import ChallengeSection from '@/components/organisms/ChallengeSection';
 import Template from '@/components/templates/Template';
-import getHeadlineNewestChallenges from '@/data/remote/strapi/collection/get-headline-newest-challenges';
+import getAllNewestChallengesByPage from '@/data/remote/strapi/collection/get-all-newest-challenges-by-page';
 import getChallengesPageContent from '@/data/remote/strapi/single/get-challenges-page-content';
 
 export default async function Challenges() {
   const challengesPageContent = await getChallengesPageContent();
-  const headlineNewestChallenges = await getHeadlineNewestChallenges();
+  const headlineNewestChallenges = await getAllNewestChallengesByPage(1);
 
   return (
     <Template>
